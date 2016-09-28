23a768221cc7feb2d4d814e309ac1296cff2834d
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 16:21:58 -0700

    Moved num1 and num2 variables below token line to avoid repetition in individual conditions

--- calculator.py
+++ calculator.py
@@ -11,38 +11,28 @@ from arithmetic import *
 # Your code goes here
 while True:
 	input_string = raw_input("<<< ")
-	token = input_string.split(" ")
+	token = input_string.split()
+	num1 = int(token[1])
+	if len(token) >= 3:
+		num2 = int(token[2])
+	
 	if token[0] == 'q':
 		break
 	elif token[0] == '+':
-		num1 = int(token[1]) 
-		num2 = int(token[2])
 		print add(num1, num2)
 	elif token[0] == '-':
-		num1 = int(token[1]) 
-		num2 = int(token[2])
 		print subtract(num1, num2)
 	elif token[0] == '*':
-		num1 = int(token[1]) 
-		num2 = int(token[2])
 		print multiply(num1, num2)
 	elif token[0] == '/':
-		num1 = int(token[1]) 
-		num2 = int(token[2])
 		print divide(num1, num2)
 	elif token[0] == 'square':
-		num1 = int(token[1]) 
 		print square(num1)
 	elif token[0] == 'cube':
-		num1 = int(token[1]) 
 		print cube(num1)
 	elif token[0] == 'power':
-		num1 = int(token[1]) 
-		num2 = int(token[2])
 		print power(num1, num2)
 	elif token[0] == 'mod':
-		num1 = int(token[1]) 
-		num2 = int(token[2])
 		print mod(num1, num2)
 	else:
 		print "We don't know what are you talking about, girl!!!!"
