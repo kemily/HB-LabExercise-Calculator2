80f6264acddd64befd9991636fed1af8af05252f
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-29 13:04:00 -0700

    For fs_arithmetic.py, we finished functions for variable entries. We made no changes to calculator.py

--- calculator.py
+++ calculator.py
@@ -9,31 +9,3 @@ from arithmetic import *
 
 
 # Your code goes here
-while True:
-	input_string = raw_input("<<< ")
-	token = input_string.split()
-	num1 = int(token[1])
-	if len(token) >= 3:
-		num2 = int(token[2])
-	
-	if token[0] == 'q':
-		break
-	elif token[0] == '+':
-		print add(num1, num2)
-	elif token[0] == '-':
-		print subtract(num1, num2)
-	elif token[0] == '*':
-		print multiply(num1, num2)
-	elif token[0] == '/':
-		print divide(num1, num2)
-	elif token[0] == 'square':
-		print square(num1)
-	elif token[0] == 'cube':
-		print cube(num1)
-	elif token[0] == 'power':
-		print power(num1, num2)
-	elif token[0] == 'mod':
-		print mod(num1, num2)
-	else:
-		print "We don't know what are you talking about, girl!!!!"
-	
--- fs_arithmetic.py
+++ fs_arithmetic.py
@@ -56,3 +56,69 @@ def new_add(*arg):
     return summ
 
 print new_add(1,2,3,4,5)
+
+def new_subtract(*arg):
+    subs = arg[0]
+    for i in arg[1:]:
+        subs = subs - i
+    return subs
+
+print new_subtract(20, 5, 6)
+
+def new_mult(*arg):
+    ans = 1
+    for i in arg:
+        ans = ans * i
+    return ans
+
+print new_mult(1,5,6)
+
+def new_divide(*arg):
+    quo = float(arg[0])
+    for i in arg[1:]:
+        quo = quo / i
+    return quo
+
+print new_divide(5,7,8,10)
+
+def new_square(*arg):
+    if len(arg) > 1:
+        return 'Sorry, you cannot square more than one number'
+    elif len(arg) == 1:
+        return arg[0] * arg[0]
+
+print new_square (2,4)
+print new_square (3)
+
+def new_cube(*arg):
+    if len(arg) > 1:
+        return 'Sorry, you cannot cube more than one number'
+    elif len(arg) == 1:
+        return arg[0] * arg[0] * arg[0]
+
+print new_cube(1,4,6)
+print new_cube(4)
+
+def new_power(*arg):
+    exp = arg[0]
+    for i in arg[1:]:
+        exp = exp ** i
+    return exp
+
+print new_power(2,2,5)
+
+
+def new_mod(*arg):
+    mod = arg[0]
+    for i in arg[1:]:
+        if mod % i == 0:
+            print 'Sorry, there is no remainder for your entries' 
+            break
+        elif mod > 0:
+            print "You are getting modulo between %s and %s" % (mod, i)
+            mod = mod % i  
+            print "The remainder is", mod  
+    return 'Goodbye!'
+
+print new_mod(120, 50, 8)
+print new_mod(120, 60, 30)
