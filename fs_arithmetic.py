a1003d3e2b5ff1830ffefa59be0b885191e6717f
vagrant <vagrant@vagrant.vm>
2016-09-29 05:29:49 +0000

    added new function substruct, that substract all the elements from the first element in the unknown given range of numbers. Added comments and docstring to it

--- fs_arithmetic.py
+++ fs_arithmetic.py
@@ -1,5 +1,5 @@
-def add(num1, num2):
-    return num1 + num2
+# def add(num1, num2):
+#     return num1 + num2
 
 
 def subtract(num1, num2):
@@ -13,7 +13,7 @@ def multiply(num1, num2):
 def divide(num1, num2):
     # Need to turn at least argument to float for division to
     # not be integer division
-    return float(num1) / float(num2) 
+    return float(num1) / float(num2)
 
 
 def square(num1):
@@ -33,26 +33,37 @@ def power(num1, num2):
 def mod(num1, num2):
     return num1 % num2
 
-#Trying functions for variable arguments below
-
-# def new_add(*arg):
-#     input_string = arg
-#     tokens = input_string.split()
-#     print tokens
-#     numbers = range(1, (len(tokens)-1))
-#     print numbers
-#     summ = 0
-#     for i in numbers:
-#         summ = summ + i
-#     return summ
 
 
-# print new_add(1,2,3,4,5)
 
 def new_add(*arg):
+    """This function takes an unknown number of arguments and return sum of them"""
     summ = 0
     for i in arg:
         summ = summ + i
     return summ
 
-print new_add(1,2,3,4,5)
+print new_add(1, 2, 3, 4, 5)
+
+def subtract(*arg):
+    """This function subtracts from the first number the rest of the numbers in the given range"""
+    #first element will be the initial value of substraction, since we substruct from it
+    subtraction = arg[0]
+    #choosing the range from the second element till the end
+    #this is what we are going to gradually substruct from the first element
+    for i in arg[1:]:
+        subtraction -= i
+    return subtraction
+
+print subtract(20, 4, 6, 8)
+
+
+
+
+
+
+
+
+
+
+
